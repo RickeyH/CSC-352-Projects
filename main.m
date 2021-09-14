e93f8ba70fdccd0f33f577e295d0886c7c47b910
rickeyhuang <huanr19@wfu.edu>
2021-09-14 13:31:28 -0400

    Final Commit

--- main.m
+++ main.m
@@ -21,3 +21,15 @@ j = 100;
 G = u(:,1:j)*s(1:j,1:j)*v(:,1:j)';
 figure, imagesc(G),colormap(gray);
 axis image;
+% Compress the image using the closest-rank 70 matrices
+k = 70;
+Gk = u(:,1:k)*s(1:k,1:k)*v(:,1:k)';
+figure, imagesc(Gk),colormap(gray);
+axis image;
+% Compute the number of elements stored in the maatrices
+% Number of elements in matrix from the full picture
+spaceFull = numel(s)+numel(u)+numel(v');
+% Number of elements in the matrix from the compressed image
+spacek = numel(s(1:k,1:k))+numel(u(:,1:k))+numel(v(:,1:k)');
+% Calculate the percent of space saved
+savedSpace = (spaceFull-spacek)./spaceFull;
