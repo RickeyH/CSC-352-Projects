fb1f2059e096a2d59de788a75429773dbd8b1628
rickeyhuang <huanr19@wfu.edu>
2021-09-14 01:23:07 -0400

    Second Commit

--- main.m
+++ main.m
@@ -1,8 +1,23 @@
- % Read the image
-chapel = imread('Chapel.jpeg');
+% Read the image
+A = imread('Chapel.jpeg');
 % Convert the image into greyscale
-chapelG= rgb2gray(chapel);
+Ag = rgb2gray(A);
 % Convert the image to double
-chapelD = double(chapelG);
+Ad = double(Ag);
 % Display the image
-figure, colormap(gray), image(chapelD);
+figure, imagesc(Ad), colormap(gray);
+axis image;
+% Return the size of the image
+dim = size(Ad);
+% SVD
+decomposition = svd(Ad);
+% Plot the singular values
+plot(decomposition,'*');
+% Compute the size of sigma
+[u,s,v] = svd(Ad);
+dims = size(s);
+% Compress the image using the closest-rank matrices
+j = 100;
+G = u(:,1:j)*s(1:j,1:j)*v(:,1:j)';
+figure, imagesc(G),colormap(gray);
+axis image;
