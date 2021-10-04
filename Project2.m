286da75f061af26ded5e72d39de268adc39ff47a
rickeyhuang <huanr19@wfu.edu>
2021-10-04 19:05:37 -0400

    Project 2 third commit

--- Project2.m
+++ Project2.m
@@ -2,6 +2,12 @@
 %% Fitting the best linear model usign the least squares
 %% Input the response data and obervations
 Years = [1761; 1767; 1771; 1773; 1776; 1779; 1783; 1785; 1788; 1791];
-KVnum = [1; 41; 111; 176; 246; 3138; 422; 474; 535; 626];
+KVnum = [1; 41; 111; 176; 246; 318; 422; 474; 535; 626];
 %% Fit the least-square linear model using the function polyfit
 linearModel = polyfit(KVnum, Years, 1);
+%% Question 2
+%% First Create the design matrix
+A = [ones(1,10); KVnum'; (KVnum.^3)']';
+%% Part 1: inverses(using SVD)
+Ainv = pinv(A);
+inerse =(inv(A'*A))*A';
