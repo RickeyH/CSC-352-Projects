c1cd8b24eff7782ed0bc5219852682b575d84030
rickeyhuang <huanr19@wfu.edu>
2021-10-04 23:36:41 -0400

    Project 2 Fourth Commit

--- Project2.m
+++ Project2.m
@@ -7,7 +7,18 @@ KVnum = [1; 41; 111; 176; 246; 318; 422; 474; 535; 626];
 linearModel = polyfit(KVnum, Years, 1);
 %% Question 2
 %% First Create the design matrix
-A = [ones(1,10); KVnum'; (KVnum.^3)']';
-%% Part 1: inverses(using SVD)
-Ainv = pinv(A);
-inerse =(inv(A'*A))*A';
+A = [ones(1,10); KVnum.'; (KVnum.^3).'].';
+%% Part 1: Inverse of A^TA
+inver = inv(A.'*A)*A.';
+sol1 = inver*Years;
+%% Part 2: Pseudoinverse
+pseudoInver = pinv(A);
+sol2 = pinv(A)*Years;
+%% Part 3: QR Factorization
+[Q,R] = qr(A);
+Qtb = Q.'*Years;
+sol3 = R\Qtb;
+%% Analyzing part 1 and part 2
+inv = inv(A.'*A);
+Pinv = pinv(A)/A.';
+Rinv = inv(R);
